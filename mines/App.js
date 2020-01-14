e1acea8e0d902ee6c37f92a3d827f74318dee9b4
Márcio Habigzang Brufatto <mhbrufatto@gmail.com>
2020-01-14 18:15:35 -0300

    Criada a lógica do campo minado.

--- mines/App.js
+++ mines/App.js
@@ -1,9 +1,57 @@
 import React, { Component } from 'react'
-import { StyleSheet, View, SafeAreaView, Text} from 'react-native'
+import { StyleSheet, View, SafeAreaView, Text, Alert} from 'react-native'
 import params from './src/params'
-import Field from './src/components/Field'
+import MineField from './src/components/MaineField'
+import { 
+  createMinedBoard, 
+  cloneBoard,
+  openField,
+  hadExplosion,
+  wonGame,
+  showMines
+} from './src/functions'
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = this.createState()
+  }
+
+  minesAmount = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return Math.ceil(cols * rows * params.difficultLevel)
+  }
+
+  createState = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return {
+      board: createMinedBoard(rows, cols, this.minesAmount()),
+      won: false,
+      lost: false
+    }
+  }
+
+  onOpenField = (row, column) => {
+    const board = cloneBoard(this.state.board)
+    openField(board, row, column)
+    const lost = hadExplosion(board)
+    const won = wonGame(board)
+
+    if (lost) {
+      showMines(board)
+      Alert.alert('Perdeuuuuu!', 'Que burroooo')
+    }
+
+    if (won) {
+      Alert.alert('Parabéns', 'Você Venceu!')
+    }
+
+    this.setState({ board, lost, won })
+  }
+
   render() {
     return (
       <SafeAreaView style={styles.safeArea}>
@@ -11,18 +59,9 @@ export default class App extends Component {
           <Text style={ styles.welcome}>Iniciando o Mines!</Text>
           <Text>Tamanho da grade:
             {params.getRowsAmount()}X{params.getColumnsAmount()}</Text>
-
-            <Field />
-            <Field opened />
-            <Field opened nearMines={1}/>
-            <Field opened nearMines={2}/>
-            <Field opened nearMines={3}/>
-            <Field opened nearMines={6}/>
-            <Field mined />
-            <Field mined opened />
-            <Field mined opened exploded />
-            <Field flagged />
-            <Field flagged opened />
+          <View style={styles.board}>
+            <MineField board={this.state.board} onOpenField={this.onOpenField}/>
+          </View>
         </View>
       </SafeAreaView>
     )
@@ -30,19 +69,16 @@ export default class App extends Component {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
   safeArea: {
     flex: 1,
     marginTop: 40,
   },
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  board: {
+    alignItems: 'center',
+    backgroundColor: '#AAA'
+  }
 })
