401067662d4c14df9ee002c9ec5cce1f05fe1b3b
Márcio Habigzang Brufatto <mhbrufatto@gmail.com>
2020-01-15 15:17:02 -0300

    Criado projeto tasks e a tela de agenda

---
+++ tasks/src/commonStyles.js
@@ -0,0 +1,13 @@
+export default {
+    fontFamily: 'Lato',
+    colors: {
+        today: '#b13b44',
+        secondary: '#FFF',
+        mainText: '#222',
+        subText: '#555'
+    },
+    safeArea: {
+        flex: 1,
+        marginTop: 40,
+    },
+}
